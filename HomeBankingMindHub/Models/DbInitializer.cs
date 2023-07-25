730aed55f9c31d5d9eb894a41c42d540e2f7ffdb
Tomtechno <69020112+Tomtechno@users.noreply.github.com>
2023-07-25 14:01:54 -0800

    Add project files.

---
+++ HomeBankingMindHub/Models/DbInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace HomeBankingMindHub.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client { Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="123v23r456"},
+                    new Client { Email = "eduardo@gmail.com", FirstName="Eduardo", LastName="Mendoza", Password="aaet342"},
+                    new Client { Email = "eren@gmail.com", FirstName="Eren", LastName="Jaeger", Password="Pasfd#a2"},
+                    new Client { Email = "mikasa12@gmail.com", FirstName="Mikasa", LastName="Ackerman", Password="o2314nnsaA%"},
+                    new Client { Email = "tomas@gmail.com", FirstName="Tomas", LastName="Uzquiano", Password="hae345b%"},
+
+                };
+
+                foreach (Client client in clients)
+                {
+                    context.Clients.Add(client);
+                }
+
+                //guardamos
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
+
